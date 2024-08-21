e72a24341c1e2e9ac655c5073c5a08a272a5e2ae
Edwin unity <eapaniagua8@misena.edu.co>
2024-08-21 11:55:51 -0500

    constantes

---
+++ Assets/Scripts/PractiPrograAvan.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PractiPrograAvan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //const
+    public const float PI = 3.1416926f;
+    
+    // solo lectura
+    public readonly int numbuerOfEnemies;
+
+    PractiPrograAvan()
+    {
+        numbuerOfEnemies = 5;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
